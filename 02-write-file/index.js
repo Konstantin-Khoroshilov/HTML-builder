1cdbe3b51996a2c399a6e4eca0324a48f4f5148c
Konstantin Khoroshilov <manly41142009@yandex.ru>
2023-05-06 20:21:42 +0300

    feat: implement solution for task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const { stdin, stdout, exit } = process;
+const path = require('path');
+const file = path.join(__dirname, 'text.txt');
+const finish = () => {
+  stdout.write('Вы были великолепны! Ваша мудрость запечатлена в строках файла text.txt и останется с потомками навеки.');
+  exit();
+}
+fs.access(file, err => {
+  if (err) {
+    fs.writeFile(
+      file,
+      '',
+      err => {
+        if (err) throw err;
+      }
+    );
+  }
+  stdout.write('Приветствуем Вас, милостивый государь!\nИзвольте ввести в консоль Ваши глубокомысленные изречения и нажать Enter:\n');
+  stdin.on('data', data => {
+    const userText = data.toString();
+    if (userText.trim() === 'exit') finish();
+    fs.appendFile(
+      file,
+      userText,
+      err => { if (err) throw err; }
+    );
+  });
+});
+process.on('SIGINT', () => {
+  finish();
+});
